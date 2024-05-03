29d5e793fd951989474bc62065fc1224fea1772f
suhara beevi <sairasuhara12345@gmail.com>
2024-05-03 17:09:53 +0530

    login page

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+    <div className='bg-slate-50'></div>
+    <img src="https://res.cloudinary.com/dpgbodkae/image/upload/v1711122784/5a7e508b-d571-46df-94d0-badf52ae6288_1711116793709442186_fgl5v2.png" alt="" className='w-1/2 h-1/2 mt-8'/>
+    </div>
+    
+  )
+}
+
+export default Login
